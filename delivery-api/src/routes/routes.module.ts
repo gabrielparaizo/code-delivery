699ff12d9ca28f26967e945e20fe962cf8e3e8d4
Gabriel Paraizo <36803487+gabrielparaizo@users.noreply.github.com>
2025-02-05 19:49:44 -0300

    feat: add driverRoutes
    In order to use this information later in our front-end, I've added some
    useful driver information

--- delivery-api/src/routes/routes.module.ts
+++ delivery-api/src/routes/routes.module.ts
@@ -3,10 +3,11 @@ import { RoutesService } from './routes.service';
 import { RoutesController } from './routes.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { MapsModule } from 'src/maps/maps.module';
+import { RoutesDriverService } from './routes-driver/routes-driver.service';
 
 @Module({
   imports: [MapsModule],
   controllers: [RoutesController],
-  providers: [RoutesService],
+  providers: [RoutesService, RoutesDriverService],
 })
 export class RoutesModule { }
